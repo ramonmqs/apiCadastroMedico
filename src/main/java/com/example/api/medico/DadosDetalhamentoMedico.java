025ae53d626bc93700a6ed073a7b39ec724b2dcc
Ramon Marques <ramonlmarques@hotmail.com>
2023-10-23 18:59:44 -0300

    atualizações de boas praticas

---
+++ src/main/java/com/example/api/medico/DadosDetalhamentoMedico.java
@@ -0,0 +1,10 @@
+package com.example.api.medico;
+
+import com.example.api.endereco.Endereco;
+
+public record DadosDetalhamentoMedico(Long id, String nome, String crm, String telefone, Especialidade especialidade, Endereco endereco) {
+
+    public DadosDetalhamentoMedico(Medico medico){
+        this(medico.getId(), medico.getNome(), medico.getCrm(), medico.getTelefone(), medico.getEspecialidade(), medico.getEndereco());
+    }
+}
